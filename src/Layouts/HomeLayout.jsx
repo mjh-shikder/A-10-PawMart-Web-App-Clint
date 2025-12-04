7f8c4e44be857ea2facdc59b7e04dfaff83cb0dc
mjh-shikder <mjhsikder@gmail.com>
2025-12-04 22:39:46 +0600

    footer added

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import MyContainer from '../components/MyContainer';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import { ToastContainer } from 'react-toastify';
 
 const HomeLayout = () => {
     return (
-        <div>
+        <div className='bg-base-200'>
+            <title>Paw Mart | Home</title>
+            <header>
+                <nav>
+                    <MyContainer>
+                        <Navbar></Navbar>
+                    </MyContainer>
+                </nav>
+            </header>
+            <main>
+                <MyContainer>
+                    <section>
             <Outlet></Outlet>
+                    </section>
+                </MyContainer>
+            </main>
+            <footer>
+                <MyContainer>
+                    <Footer></Footer>
+                </MyContainer>
+            </footer>
+        <ToastContainer></ToastContainer>
         </div>
     );
 };
