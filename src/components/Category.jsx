52f1d09443da64b9c21684fcb3108ad7f89816de
mjh-shikder <mjhsikder@gmail.com>
2025-12-05 07:28:13 +0600

    all categorys added

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -70,6 +70,75 @@ const Category = () => {
           </div>
             </Link>
           </SwiperSlide>
+          <SwiperSlide className="rounded-2xl ">
+            <Link>
+               <div className="relative w-full h-full">
+            <img
+              className="absolute inset-0 w-full h-full object-cover object-center 
+                 md:object-[50%_28%] lg:object-[50%_45%]"
+              src="https://images.unsplash.com/photo-1696507398173-863ae397a059?q=80&w=765&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+
+            <div className="absolute inset-0 bg-black/45" />
+
+            <div className="relative z-10 flex h-full items-center  md:items-center md:justify-start md:pb-0">
+              <div className="max-w-7xl mx-auto px-6 md:px-12 lg:px-16">
+                <h2 className="text-4xl md:text-2xl lg:text-3xl font-bold text-white drop-shadow-lg ">
+                  Pet Food
+                </h2>
+                
+              </div>
+            </div>
+          </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide className="rounded-2xl ">
+            <Link>
+               <div className="relative w-full h-full">
+            <img
+              className="absolute inset-0 w-full h-full object-cover object-center 
+                 md:object-[50%_28%] lg:object-[50%_45%]"
+              src="https://images.unsplash.com/photo-1589924749359-9697080c3577?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+
+            <div className="absolute inset-0 bg-black/45" />
+
+            <div className="relative z-10 flex h-full items-center  md:items-center md:justify-start md:pb-0">
+              <div className="max-w-7xl mx-auto px-6 md:px-12 lg:px-16">
+                <h2 className="text-4xl md:text-2xl lg:text-3xl font-bold text-white drop-shadow-lg ">
+                  Accessories
+                </h2>
+                
+              </div>
+            </div>
+          </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide className="rounded-2xl ">
+            <Link>
+               <div className="relative w-full h-full">
+            <img
+              className="absolute inset-0 w-full h-full object-cover object-center 
+                 md:object-[50%_28%] lg:object-[50%_45%]"
+              src="https://images.unsplash.com/photo-1631511231050-24f8dba10537?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+
+            <div className="absolute inset-0 bg-black/45" />
+
+            <div className="relative z-10 flex h-full items-center  md:items-center md:justify-start md:pb-0">
+              <div className="max-w-7xl mx-auto px-6 md:px-12 lg:px-16">
+                <h2 className="text-4xl md:text-2xl lg:text-3xl font-bold text-white drop-shadow-lg text-center">
+                  Pet Care
+                </h2>
+                
+              </div>
+            </div>
+          </div>
+            </Link>
+          </SwiperSlide>
           
         </Swiper>
       </div>
